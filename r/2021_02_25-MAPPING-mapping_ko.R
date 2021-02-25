9d1ddf3d9f7db22691213533771fccc3f51d6788
mirnavazquez <mirnavrl@gmail.com>
2021-02-25 12:37:59 -0600

    I updated the rad_ko function and create the mapping_ko

---
+++ r/2021_02_25-MAPPING-mapping_ko.R
@@ -0,0 +1,77 @@
+library(KEGGREST)
+library(tibble)
+library(dplyr)
+library(stringr)
+library(tidyr)
+
+mapping_ko<-function(ko_abundance_table){
+  #########################  KEGG links ############################
+  module_link<- keggLink("ko", "module") %>%
+    enframe() %>%
+    rename(KO = value) %>%
+    rename(Module = name) %>%
+    mutate(Module=str_remove_all(Module, "md:")) %>%
+    mutate(KO=str_remove_all(KO, "ko:")) 
+  pathway_link <- keggLink("ko", "pathway") %>%
+    enframe() %>%
+    rename(KO = value) %>%
+    rename(Pathway = name) %>%
+    filter(!str_detect(Pathway, "ko")) %>%
+    mutate(Pathway=str_remove_all(Pathway, "path:")) %>%
+    mutate(KO=str_remove_all(KO, "ko:"))  
+  enzyme_link<-keggLink("ko", "enzyme") %>%
+    enframe() %>%
+    rename(KO = value) %>%
+    rename(Enzyme = name ) %>%
+    mutate(KO=str_remove_all(KO, "ko:")) 
+  #########################  KEGG lists ############################
+  modules_list<-keggList("module") %>%
+    enframe() %>%
+    rename(Module_description = value) %>%
+    rename(Module = name ) %>%
+    mutate(Module=str_remove_all(Module, "md:")) 
+  pathway_list<-keggList("pathway")%>%
+    enframe() %>%
+    rename(Pathway_description = value) %>%
+    rename(Pathway = name ) %>%
+    mutate(Pathway=str_remove_all(Pathway, "path:")) 
+  ko_list<-keggList("ko") %>%
+    enframe() %>%
+    rename(KO_description = value) %>%
+    rename(KO = name ) %>%
+    separate(KO_description, 
+           c("Genes", "Gene_description"), sep="; ") %>%
+    mutate(KO=str_remove_all(KO, "ko:"))
+  #########################  KO master #############################
+  KO_master<-left_join(ko_list, pathway_link, by="KO") %>%
+    left_join(pathway_list, by="Pathway") %>%
+    left_join(module_link, by="KO") %>%
+    left_join(modules_list, by="Module") %>%
+    left_join(enzyme_link, by="KO") %>%
+    select(Module, Module_description, Pathway, 
+         Pathway_description, KO, Genes, 
+         Gene_description, Enzyme) %>%
+    distinct()
+  ######################  Remove data ##############################
+  rm(module_link, pathway_link, enzyme_link, modules_list, 
+    pathway_list, ko_list)
+  ###################################################################
+  ko_abundance_table %>%
+  left_join(KO_master, by="KO") %>%
+    select(Module, Module_description, Pathway, 
+         Pathway_description, KO, Abundance, Genes, 
+         Gene_description, Enzyme,Bin_name, 
+         Scaffold_name) %>%
+    distinct()%>%
+    pivot_wider(names_from = Bin_name, 
+                values_from = Abundance, values_fill = 0)
+}
+
+
+b<-mapping_ko(a)
+
+
+
+
+
+
---
+++ r/2021_02_25-READ-read_ko.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(tidyr)
+library(readr)
+library(stringr)
+read_ko<-function(ko_result){
+  KO_raw<-read_table2(ko_result, col_names = F) %>%
+    filter(!str_detect(X1, '#')) %>%
+    select(X2, X3) %>%
+    rename(Bin_name = X2) %>%
+    rename(KO = X3) %>%
+    separate(Bin_name, c("Bin_name", "Scaffold_name"), sep = "[_|-][s|S]caffold") %>%
+    mutate(Scaffold_name = paste0( "scaffold", Scaffold_name), Scaffold_name)
+  
+  KO_abundance<-KO_raw %>%
+    group_by(Bin_name) %>%
+    distinct() %>%
+    count(KO) %>%
+    rename(Abundance = n)
+  
+  left_join(KO_raw, KO_abundance, by=c("Bin_name", "KO")) %>%
+    distinct()
+
+}
+  
+a<-read_ko("data/B113_G9_result.txt")
+
+
