1618a8a65078b7fdea39eddb4c1dd6f8a744ed6e
mirnavazquez <mirnavrl@gmial.com>
2021-04-01 15:34:29 -0500

    updated variable names

--- r/2021_02_25-PLOT-plot_heatmap_abundace.R
+++ r/2021_02_25-PLOT-plot_heatmap_abundace.R
@@ -1,69 +1,74 @@
-#' @title Heatmap plot of KO abundance within each Bin.
+#' @title Heatmap plot of KO abundance within each bin.
 #' @description Creates a heatmap of KO abundance.
-#' @param tabble_ko a data frame object, created with the mapping_ko or get_subset_* functions.
-#' @param other_data a data frame object containing metadata information.
-#' @param plot_ano is a column name indicating the annotation if rows. This column name comes from the mapping_ko or get_subset_* object.
-#' @param plot_medata is a column name indicating the annotation if cols. This column name comes from the metadata object.
-#' @details This function is part of a package used for the analysis of bins metabolism.
+#' @param tibble_ko a data frame object, created with the mapping_ko or 
+#' get_subset_* functions.
+#' @param data_experiment a data frame object containing metadata information.
+#' @param metabolism_col is a column name indicating the annotation if rows. 
+#' This column name comes from the mapping_ko or get_subset_* object.
+#' @param experiment_col is a column name indicating the annotation of cols. 
+#' This column name comes from the metadata object.
+#' @details This function is part of a package used for 
+#' the analysis of bins metabolism.
 #' @import pheatmap rlang
 #' @examples
 #' plot_heatmap_abundance(ko_bin_mapp, metadata, Module, Sample_site)
 #' @export
-plot_heatmap_abundance<-function(tabble_ko, 
-                                 other_data,
-                                 plot_ano,
-                                 plot_medata){
-  ############################ quoting ##############################
-  plot_ano_enquo <- enquo(plot_ano)
-  plot_medata_enquo <- enquo(plot_medata)
-  plot_ano_label <- as_label(plot_ano_enquo)
-  ###################################################################
-  tabble_ko_colnames<-colnames(tabble_ko)
+plot_heatmap_abundance<-function(tibble_ko, 
+                                 data_experiment,
+                                 metabolism_col,
+                                 experiment_col){
+  # Quoting ---------------------------------------------------------------####
+  metabolism_col_enquo <- enquo(metabolism_col)
+  experiment_col_enquo <- enquo(experiment_col)
+  metabolism_col_label <- as_label(metabolism_col_enquo)
+  # Sorting col names -----------------------------------------------------####
+  tibble_ko_colnames<-colnames(tibble_ko)
   paths<-c("Module", "Module_description", "Pathway", 
            "Pathway_description", "Genes", 
            "Gene_description", "Enzyme", "KO", "Cycle", "Pathway_cycle",
            "Detail_cycle", "rbims_pathway", "rbims_sub_pathway")
-  tabble_ko_colnames_2 <- tabble_ko_colnames[tabble_ko_colnames %in% paths]
-  tabble_ko_colnames_3 <- tabble_ko_colnames_2[!tabble_ko_colnames_2 %in% "KO"]
-  both_paths<-c("KO", plot_ano_label)
-  tabble_ko_colnames_4 <- tabble_ko_colnames_3[!tabble_ko_colnames_3 %in% plot_ano_label]
-  ########## Create pivot tables #############
-  metabolism.wide <- tabble_ko %>%
-    select(-all_of(tabble_ko_colnames_3)) %>%
+  tibble_ko_colnames_2 <- tibble_ko_colnames[tibble_ko_colnames %in% paths]
+  tibble_ko_colnames_3 <- tibble_ko_colnames_2[!tibble_ko_colnames_2 %in% "KO"]
+  both_paths<-c("KO", metabolism_col_label)
+  tibble_ko_colnames_4 <- tibble_ko_colnames_3[!tibble_ko_colnames_3 
+                                               %in% metabolism_col_label]
+  # Removing cols ---------------------------------------------------------####
+  heatmap_table <- tibble_ko %>%
+    select(-all_of(tibble_ko_colnames_3)) %>%
     distinct() %>%
     column_to_rownames("KO")
-  ################ Row metadata ##############
-  metadata_hydro_path<- tabble_ko %>%
-    select( "KO", {{ plot_ano_enquo }}) %>%
+  # Extracting metabolism -------------------------------------------------####
+  metabolism_order<- tibble_ko %>%
+    select( "KO", {{ metabolism_col_enquo }}) %>%
     drop_na() %>%
     distinct(.data$KO, .keep_all = T) %>%
     column_to_rownames("KO") %>%
-    arrange({{ plot_ano_enquo }})
-  ################ Row metadata ##############
+    arrange({{ metabolism_col_enquo }})
+  # Extracting experiment -------------------------------------------------####
   data_to_select<-c("Module", "Module_description", "Pathway", 
                     "Pathway_description", "Genes", 
                     "Gene_description", "Enzyme", "Cycle", "Pathway_cycle",
                     "Detail_cycle", "KO", "rbims_pathway", "rbims_sub_pathway")
-  metadata_column<- tabble_ko %>% 
+  experiment_order<- tibble_ko %>% 
     pivot_longer(cols = -data_to_select, values_to = "Abundance",
                  names_to="Bin_name") %>%
     distinct() %>%
-    left_join(other_data, by="Bin_name") %>%
-    select( "Bin_name", {{ plot_medata_enquo }}) %>%
+    left_join(data_experiment, by="Bin_name") %>%
+    select( "Bin_name", {{ experiment_col_enquo }}) %>%
     drop_na() %>%
     distinct(.data$Bin_name, .keep_all = T) %>%
     column_to_rownames("Bin_name") %>%
-    arrange({{ plot_medata_enquo }})
-  #######  Reorder the names of rows ##############################
-  sub_samp_ordered <- metabolism.wide[rownames(metadata_hydro_path),]
-  sub_samp_ordered_2 <- sub_samp_ordered[,rownames(metadata_column)]
-  
-  metabolism.wide_pheatmap<-pheatmap(sub_samp_ordered_2, 
+    arrange({{ experiment_col_enquo }})
+  # Order table -----------------------------------------------------------####
+  sub_samp_ordered <- heatmap_table[rownames(metabolism_order),]
+  sub_samp_ordered_2 <- sub_samp_ordered[,rownames(experiment_order)]
+  # Plot ------------------------------------------------------------------####
+  heatmap_table_pheatmap<-pheatmap(sub_samp_ordered_2, 
                                      scale = "row",
-                                     annotation_row = metadata_hydro_path,
-                                     annotation_col = metadata_column,
+                                     annotation_row = metabolism_order,
+                                     annotation_col = experiment_order,
                                      cluster_rows = F,
                                      cluster_cols = F)
   
-  return(metabolism.wide_pheatmap)
+  return(heatmap_table_pheatmap)
 }
