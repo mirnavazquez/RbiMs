b6d5a7fee17106a47b2675169161ff788a71e3b0
Bryan Vernon <bryan.vernon@hotmail.com>
2021-06-22 17:13:09 -0500

    Modified read_ko so it can read multiple .txt files and concatenate them. Also fixed 1 typo.

--- R/2021_02_25-READ-read_ko.R
+++ R/2021_02_25-READ-read_ko.R
@@ -2,21 +2,22 @@
 #' @description read_ko calculates the abundance of each KO within the 
 #' bins based on the KofamScan or KofamKoala output.
 #' @usage read_ko(data_kofam=NULL, data_kaas=NULL, data_interpro=NULL)
-#' @param data_kofam a data frame with 5 columns. Contigs are expected to 
-#' indicate in their names the bin name follow by the scaffold name 
-#' divided by an "_": bin_scaffoldXX.
+#' @param data_kofam a path where KofamScan/KofamKoala output data are. They 
+#' should have the extension .txt and it is recommended all files in the path 
+#' are the ones that need to be read. Output data should have 5 columns with the
+#' bin names followed by the scaffold name divided by an “_”: bin_scaffoldXX.
 #' @param data_kaas a data frame with 2 columns. Contigs are expected to 
-#' indicate in their names the bin name follow by the scaffold name 
+#' indicate in their names the bin name followed by the scaffold name 
 #' divided by an "_": bin_scaffoldXX. 
 #' @param data_interpro a data frame output of read_interpro. This
 #' argument is used within mapping_KO.
 #' @details This function is part of a package used for the analysis 
 #' of bins metabolism.
-#' @import dplyr tidyr readr stringr rlang
+#' @import dplyr tidyr readr stringr rlang purrr
 #' @importFrom utils read.table
 #' @examples
 #' \dontrun{
-#' read_ko("KEGG_bins.txt")
+#' read_ko("C:/Users/bins")
 #' }
 #' @export
 read_ko<-function(data_kofam=NULL, 
@@ -25,15 +26,17 @@ read_ko<-function(data_kofam=NULL,
   # Kofam_fun -------------------------------------------------------------####
   if( is.null(data_kofam) == F && is.null(data_kaas) == F || 
       is.null(data_kofam) == F){
+    files <- dir(path = data_kofam ,pattern ="*.txt")
     table_Kofam<-suppressWarnings(
-      suppressMessages(
-        read_table2(data_kofam, col_names = F) %>%
-          filter(str_detect(.data$X1, '\\*')) %>%
-          filter(!str_detect(.data$X1, '#')) %>%
-          select(.data$X2, .data$X3) %>%
-          rename(Bin_name = .data$X2) %>%
-          rename(KO = .data$X3)
-      ))
+      suppressMessages( 
+        files %>%
+      map(read_table2, col_names = F) %>%
+      reduce(rbind)  %>%
+      filter(str_detect(.data$X1, '\\*')) %>%
+      select(.data$X2, .data$X3) %>%
+      rename(Bin_name = .data$X2) %>%
+      rename(KO = .data$X3)
+    ))
   }
   
   # Kaas_fun --------------------------------------------------------------####
