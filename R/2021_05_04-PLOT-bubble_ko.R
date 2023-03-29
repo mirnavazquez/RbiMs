1042c760e15199da6df8ced530fdf3b9368e59f2
Lango00 <mildredlango990831@gmail.com>
2023-03-29 01:25:46 +0000

    warning color pallet, check

--- R/2021_05_04-PLOT-bubble_ko.R
+++ R/2021_05_04-PLOT-bubble_ko.R
@@ -71,6 +71,7 @@ bubble_ko<-function(tibble_ko,
   # Checking the color ----------------------------------------------------####
   if(is.null(color_pallet) == T){
     color_pallet<-as.vector(cols25(20))
+    warning("if your vector is >25, choose another color pallet")
   }
   # Checking the size -----------------------------------------------------####
   if(is.null(range_size) == T){
