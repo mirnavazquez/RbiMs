1fa2a8c6d8d220aab0d123ec8f310a49af859ae0
Karla López <karlita@192.168.68.113>
2024-01-30 20:34:37 -0600

    Addition of the missing parts of read_dbcan eg. what happens when there isn't an input for e_cami. Improvement of the data frame visualization.

--- R/READ-read_dbCAN3.R
+++ R/READ-read_dbCAN3.R
@@ -22,68 +22,98 @@
 
 read_dbcan3<-function(dbcan_path, write=FALSE, profile=TRUE){
   ruta_dbcan<-dbcan_path
+  
   # Load all the data tables results ---------------------------------------####
   lapply_read_delim_bind_rows <- function(path, pattern = "*overview.txt"){
     files = list.files(path, pattern, full.names = TRUE)
-    lapply(files, read.delim, check.names=F) %>% bind_rows()
+    lapply(files, read.delim, check.names=F) %>% 
+      bind_rows()
   }
-  dbcan_df<-suppressWarnings(lapply_read_delim_bind_rows(ruta_dbcan))
+  dbcan_df<-suppressWarnings(lapply_read_delim_bind_rows(ruta_dbcan)) 
+  
+  
   # Reading data ----------------------------------------------------------####
   dbcan_df_format<- suppressWarnings(
     dbcan_df %>%
-      filter( .data$`#ofTools` >1) %>%
-      rename(Bin_name=.data$`Gene ID`) %>%
-      mutate( hmmer2=str_replace_all(.data$HMMER, "[[:punct:]]",  "\t")) %>%
+      clean_names() %>%
+      rename(Number_of_Tools = number_of_tools)  %>%
+      filter(Number_of_Tools > 1)  %>%
+      rename(Bin_name = gene_id) %>%
+      mutate( hmmer2=str_replace_all(.data$hmmer, "[[:punct:]]",  "\t")) %>%
       separate(.data$hmmer2, c("dbNamesHMM"),  sep="\t") %>%
-      mutate(ecami2=str_replace_all(.data$eCAMI, "[[:punct:]]", "\t")) %>%
+      mutate(diamond2=str_replace_all(.data$diamond, "[[:punct:]]", "\t")) %>%
+      separate(.data$diamond2, c("dbNamesdiamond"), sep="\t"))
+
+  ###################################### e_cami ##################################  
+  if (is.null(dbcan_df_format$e_cami)) { 
+    df_format <-dbcan_df_format %>%
+      unite("dbCAN_names", dbNamesHMM, dbNamesdiamond, sep="_", remove = F) %>%  
+      mutate(dbCAN_names=str_replace_all(.data$dbCAN_names, "^_", "")) %>%
+      separate(.data$dbCAN_names, c("dbCAN_names"), sep="_") %>%
+      mutate(dbCAN = case_when(
+        str_detect(.data$dbCAN_names, "CBM") ~ "carbohydrate-binding module [CBM]",
+        str_detect(.data$dbCAN_names, "CE") ~  "carbohydrate esterases [CEs]",
+        str_detect(.data$dbCAN_names, "GH") ~  "glycoside hydrolases [GHs]",
+        str_detect(.data$dbCAN_names, "GT") ~ "glycosyltransferases [GTs]",
+        str_detect(.data$dbCAN_names, "PL") ~ "polysaccharide lyases [PLs]",
+        str_detect(.data$dbCAN_names, "AA") ~ "auxiliary ativities [AAs]")
+        )  %>%
+      mutate_if(is.character, str_trim) %>%
+      dplyr::select(.data$Bin_name, .data$dbCAN_names, domain_name = .data$dbCAN, 
+                    .data$signalp) %>%
+      calc_abundance(analysis = "dbCAN", col_rename = "dbCAN_names")  %>% 
+      dplyr::select(-.data$Scaffold_name, .data$dbCAN_names) %>% 
+      group_by(.data$Bin_name, dbCAN_family = .data$dbCAN_names, 
+               .data$domain_name, .data$signalp) %>% 
+      summarise_if(is.numeric, sum)
+    
+  } else {
+    df_format <-dbcan_df_format %/%
+      mutate(ecami2=str_replace_all(.data$e_cami, "[[:punct:]]", "\t")) %>%
       separate(.data$ecami2, c("dbNameseCAMI"), sep="\t") %>%
-      mutate(diamond2=str_replace_all(.data$DIAMOND, "[[:punct:]]", "\t")) %>%
-      separate(.data$diamond2, c("dbNamesdiamond"), sep="\t") %>%
-      unite("dbCAN_names", .data$dbNamesHMM, .data$dbNameseCAMI,  .data$dbNamesdiamond, 
+      unite("dbCAN_names", dbNamesHMM, dbNamesdiamond, dbNameseCAMI, 
             sep="_", remove = F) %>%  
       mutate(dbCAN_names=str_replace_all(.data$dbCAN_names, "^_", "")) %>%
       separate(.data$dbCAN_names, c("dbCAN_names"), sep="_") %>%
-      mutate(dbCAN = case_when(str_detect(.data$dbCAN_names, "CBM") ~ 
-                                 "carbohydrate-binding module [CBM]",
-                               str_detect(.data$dbCAN_names, "CE") ~ 
-                                 "carbohydrate esterases [CEs]",
-                               str_detect(.data$dbCAN_names, "GH") ~ 
-                                 "glycoside hydrolases [GHs]",
-                               str_detect(.data$dbCAN_names, "GT") ~ 
-                                 "glycosyltransferases [GTs]",
-                               str_detect(.data$dbCAN_names, "PL") ~ 
-                                 "polysaccharide lyases [PLs]")) %>%
+      mutate(dbCAN = case_when(
+        str_detect(.data$dbCAN_names, "CBM") ~ "carbohydrate-binding module [CBM]",
+        str_detect(.data$dbCAN_names, "CE") ~  "carbohydrate esterases [CEs]",
+        str_detect(.data$dbCAN_names, "GH") ~  "glycoside hydrolases [GHs]",
+        str_detect(.data$dbCAN_names, "GT") ~ "glycosyltransferases [GTs]",
+        str_detect(.data$dbCAN_names, "PL") ~ "polysaccharide lyases [PLs]",
+        str_detect(.data$dbCAN_names, "AA") ~ "auxiliary ativities [AAs]")
+        )  %>%
       mutate_if(is.character, str_trim) %>%
-      dplyr::select(.data$Bin_name, .data$dbCAN_names, domain_name=.data$dbCAN, .data$Signalp ) %>%
-      calc_abundance(analysis = "dbCAN") %>% 
-      dplyr::select(-.data$Scaffold_name)) %>% group_by(
-        .data$Bin_name,.data$dbCAN,  .data$domain_name, .data$Signalp) %>% summarise_if(is.numeric, sum) #juntando
-  
+      dplyr::select(.data$Bin_name, .data$dbCAN_names, 
+                    domain_name = .data$dbCAN, .data$signalp) %>%
+      calc_abundance(analysis = "dbCAN", col_rename = "dbCAN_names")  %>% 
+      dplyr::select(-.data$Scaffold_name, .data$dbCAN_names) %>% 
+      group_by(.data$Bin_name, dbCAN_family = .data$dbCAN_names, 
+               .data$domain_name, .data$signalp) %>% 
+      summarise_if(is.numeric, sum)
+
+    }
+
   # Reformating data ----------------------------------------------------------####
   
-  dbcan_df_reformat <-dbcan_df_format %>%dplyr::select(
-    -.data$Signalp) %>%
-    mutate(domain_name = case_when(str_detect(.data$dbCAN, "CBM") ~ 
-                                     "carbohydrate-binding module [CBM]",
-                                   str_detect(.data$dbCAN, "CE") ~ 
-                                     "carbohydrate esterases [CEs]",
-                                   str_detect(.data$dbCAN, "GH") ~ 
-                                     "glycoside hydrolases [GHs]",
-                                   str_detect(.data$dbCAN, "GT") ~ 
-                                     "glycosyltransferases [GTs]",
-                                   str_detect(.data$dbCAN, "PL") ~ 
-                                     "polysaccharide lyases [PLs]")) %>% group_by(
-                                       .data$dbCAN, .data$Bin_name, .data$domain_name) %>% summarize_if(
-                                         is.numeric, sum) %>%   pivot_wider(
-                                           names_from = "Bin_name", values_from = "Abundance") %>% ungroup() %>% mutate_if(
-                                             is.numeric, ~replace(., is.na(.), 0)) 
+  dbcan_df_reformat <-df_format %>%
+    dplyr::select(-.data$signalp, .data$dbCAN_family) %>%
+    group_by(.data$Bin_name, .data$dbCAN_family, .data$domain_name) %>% 
+    summarize_if(is.numeric, sum) %>%   
+    pivot_wider(names_from = "Bin_name", values_from = "Abundance") %>% 
+    ungroup() %>% 
+    mutate_if(is.numeric, ~replace(., is.na(.), 0)) 
   
   # Messages --------------------------------------------------------------####
   initial<-dim(dbcan_df)
-  final<-dim(dbcan_df %>%
-               filter( .data$`#ofTools` >1))
-  signals<- dbcan_df %>% group_by(.data$Signalp) %>% count() 
-  signals2<- dbcan_df_format %>% group_by(.data$Signalp) %>% count() 
+  final<-dim(dbcan_df_format %>%
+               filter(Number_of_Tools >1))
+  signals<- df_format %>% 
+    group_by(.data$signalp) %>% 
+    count() 
+  signals2<- df_format %>% 
+    group_by(.data$signalp) %>% 
+    count() 
   
   
   print(paste0("Input Genes = " , initial[1]))
@@ -92,26 +122,25 @@ read_dbcan3<-function(dbcan_path, write=FALSE, profile=TRUE){
   print(paste0("Number of genes with signals = " , sum(signals[-1,]$n)))
   print(paste0("Number of genes with signals that passed filtering = " , sum(signals2[-1,]$n)))
   
-# Profile or not --------------------------------------------------------------####
+  # Profile or not --------------------------------------------------------------####
   
   if(isTRUE(profile)){
     output<-dbcan_df_reformat
   } else{
-    output<-dbcan_df_format
+    output<-df_format 
   }
-
-
-# Write data or not --------------------------------------------------------------####
-
+  
+  
+  # Write data or not --------------------------------------------------------------####
+  
   if(isTRUE(write)){
     write_tsv(output, paste0("dbcan_output_", format(Sys.time(), "%b_%d_%X"), ".tsv"))
   }
- else{
-   return(output)
- }
+  else{
+    return(output)
+  }
   
   # Return ----------------------------------------------------------------####
   return(output)
+  
 }
-
-
