0bf9f8a079aa03c405e6185dfe71b13eaf46a8a4
Karla López <karlita@192.168.68.114>
2024-02-23 14:45:24 -0600

    Added the option to read multiple .tsv files in a directory

--- R/READ-read_interpro.R
+++ R/READ-read_interpro.R
@@ -1,10 +1,10 @@
 #' @title Extract abundance profile of InterProScan output.
 #' @description Reads a table object created with InterProScan and generates
-#' a profile table of abundance with the hits of the KEGG, PFAM or 
+#' a profile table of abundance with the hits of the KEGG, Pfam or 
 #' INTERPRO databases. The output of KEGG database can be used within 
 #' mapping_ko.
-#' @usage read_interpro(data_interpro, 
-#' database=c("KEGG", "Pfam", "INTERPRO",
+#' @usage read_interpro (data_interpro, 
+#' database = c("KEGG", "Pfam", "INTERPRO",
 #' "TIGRFAM", "SUPERFAMILY", "SMART", "SFLD", "ProSiteProfiles",
 #' "ProSitePatterns", "ProDom", "PRINTS", "PIRSF", 
 #' "MobiDBLite","Hamap", "Gene3D", "Coils", "CDD"), profile = TRUE)
@@ -19,24 +19,27 @@
 #' metabolism.
 #' @import tibble dplyr stringr tidyr janitor rlang
 #' @examples
-#' \dontrun{
-#' read_interpro(data_interpro="inst/extdata/Interpro_test.tsv", database="INTERPRO", 
+#' # read_interpro (data_interpro = "inst/extdata/Interpro_test.tsv", database = "INTERPRO", 
 #' profile = F)
 #' }
 #' @export
-read_interpro<-function(data_interpro, 
+read_interpro<-function(data_interpro = NULL, 
                         database=c("KEGG", "Pfam", "INTERPRO", "TIGRFAM", 
                                    "SUPERFAMILY", "SMART", "SFLD", "ProSiteProfiles",
                                    "ProSitePatterns", "ProDom", "PRINTS", "PIRSF", 
                                    "MobiDBLite","Hamap", "Gene3D", "Coils", "CDD"),
                         profile=TRUE){
+  ruta_interpro <- data_interpro
   possible_databases<-c("TIGRFAM", "SUPERFAMILY", "SMART", "SFLD", "ProSiteProfiles",
                         "ProSitePatterns", "ProDom", "PRINTS", "PIRSF", "MobiDBLite",
                         "Hamap", "Gene3D", "Coils", "CDD", "Pfam")
+  if(database == "PFAM"){
+    stop("P letter must be in capital followed by lower-case letters e.g 'Pfam'")
+  }
   if(database == "KEGG") {
-    # Extract KEGG----------------------------------------------####
-    table_interpro<-suppressWarnings(
-      suppressMessages(read_delim(data_interpro,
+    # Extract KEGG----------------------------------------------#### Aquí también 
+     table_interpro<-suppressWarnings(
+      suppressMessages(read_delim(ruta_interpro, #41 y 42 es donde esta leyendo una sola tabla 
                                   delim="\t", col_names = F) %>%
                          drop_na(.data$X12) %>%
                          select(.data$X1, .data$X15) %>%
@@ -61,23 +64,26 @@ read_interpro<-function(data_interpro,
     } 
     interpro<- table_interpro %>%
       mutate(Enzyme=str_replace_all(.data$Enzyme, "^", "ec:"))
-  } else if (database  %in% possible_databases) {
-    # Extract other databases ---------------------------------------------####
-    table_interpro_1<-suppressWarnings(
-      suppressMessages(read_delim(data_interpro,
-                                  delim="\t", col_names = F) %>%
-                         drop_na(.data$X12) %>%
-                         filter(.data$X4 == database ) %>%
-                         select(.data$X1, .data$X5, .data$X6) %>%
-                         distinct() %>%
-                         rename(Bin_name = .data$X1) %>%
-                         rename(!!database := .data$X5) %>%
-                         rename(domain_name = .data$X6) 
-      ))
-    
-    table_interpro<-table_interpro_1 %>%
-      calc_abundance(analysis = database, col_rename = database)
-    
+  } else if (database  %in% possible_databases) { 
+    # Extract other databases ---------------------------------------------#### Mas o menos por aqui
+    lapply_read_delim_bind_rows <- function(path, pattern = "*.tsv"){
+      files <- list.files(path, pattern ="*.tsv", full.names = TRUE, recursive = T)
+      lapply(files, read_tsv, col_names=F) %>% 
+        bind_rows()
+    }
+    final_files <- suppressWarnings(lapply_read_delim_bind_rows(ruta_interpro)) %>%
+        suppressWarnings() %>%
+        suppressMessages() %>%
+        drop_na(.data$X12) %>%
+        filter(.data$X4 == database ) %>%
+        select(.data$X1, .data$X5, .data$X6) %>%
+        distinct() %>%
+        rename(Bin_name = .data$X1) %>%
+        rename(!!database := .data$X5) %>%
+        rename(domain_name = .data$X6) %>%
+        calc_abundance(analysis = database, col_rename = database)
+    table_interpro <- final_files 
+
     if(isTRUE(profile)){
       interpro<-table_interpro %>%
         select(-.data$Scaffold_name) %>%
@@ -85,32 +91,38 @@ read_interpro<-function(data_interpro,
         pivot_wider(names_from= "Bin_name", values_from="Abundance", 
                     values_fill = 0)
     } else{
-      interpro<-table_interpro
+      
+      interpro <- table_interpro
     }
   } else if (database == "INTERPRO") {
     # Extract Interpro ----------------------------------------------------####
-    table_interpro_1<-suppressWarnings(
-      suppressMessages(read_delim(data_interpro,
-                                  delim="\t", col_names = F) %>%
-                         drop_na(.data$X12) %>%
-                         select(.data$X1, .data$X12, .data$X13) %>%
-                         distinct() %>%
-                         rename(Bin_name = .data$X1) %>%
-                         rename(Interpro = .data$X12) %>%
-                         rename(domain_name = .data$X13) 
-      ))
-    
-    table_interpro<-table_interpro_1 %>%
-      calc_abundance(analysis = "INTERPRO", col_rename = "INTERPRO")
+    lapply_read_delim_bind_rows <- function(path, pattern = "*.tsv"){
+      files <- list.files(path, pattern ="*.tsv", full.names = TRUE, recursive = T)
+      lapply(files, read_tsv, col_names=F) %>% 
+        bind_rows()
+    }
+    final_files <- suppressWarnings(lapply_read_delim_bind_rows(ruta_interpro)) %>%
+        suppressWarnings() %>%
+        suppressMessages() %>%
+        drop_na(.data$X12) %>%
+        select(.data$X1, .data$X12, .data$X13) %>%
+        distinct() %>%
+        rename(Bin_name = .data$X1) %>%
+        rename(Interpro = .data$X12) %>%
+        rename(domain_name = .data$X13) %>%
+        calc_abundance(analysis = "INTERPRO", col_rename = "INTERPRO")
+     table_interpro_1 <-final_files
+
     
     if(isTRUE(profile)){
-      interpro<-table_interpro %>%
+      interpro<-table_interpro_1 %>%
         select(-.data$Scaffold_name) %>%
         distinct() %>%
         pivot_wider(names_from= "Bin_name", values_from="Abundance", 
                     values_fill = 0)
     } else{
-      interpro<-table_interpro
+      
+      interpro <- table_interpro_1
     }
   }
   # Return ----------------------------------------------------------------####
