7b0f88353989f264974f890249db504fff7f7029
Stephanie Elizabeth Hereira Pacheco <shereirap@gmail.com>
2025-07-16 13:13:42 -0600

    webpage and variables

---
+++ R/zzz.R
@@ -0,0 +1,7 @@
+# zzz.R
+utils::globalVariables(c(
+  "Bin_name", "tmp", "domain_name", "number_of_tools", "Number_of_Tools",
+  "gene_id", "dbNamesHMM", "dbNamesdiamond", "dbCAN_names", "dbCAN",
+  "signalp", "Scaffold_name", "e_cami", "ecami2", "dbNameseCAMI",
+  "after_hyphen", "species_name", "peptidase_code"
+))
