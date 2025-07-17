236937c8b69813b136601f2c3e87ea6722108d96
Karla Paola López Martínez <142835080+KarlaaPLM@users.noreply.github.com>
2025-07-16 23:25:53 -0600

    Update test-read_interpro.R
    updated read_interpro test

--- tests/testthat/test-read_interpro.R
+++ tests/testthat/test-read_interpro.R
@@ -2,6 +2,6 @@ test_that("EL STOP FUNCIONA", {
   path <- system.file("extdata", "Interpro_test.tsv", package = "rbims")
   expect_error(
     read_interpro(data_interpro = path, database = "PFAM", profile = TRUE),
-    regexp = "Pfam"
+    regexp = "P letter must be in capital followed by lower-case letters e.g 'Pfam'"
   )
 })
